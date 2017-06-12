eb482691d9bc32fafd4b56f2d0f1138a07c04e70
mshepard <mshepard@andinc.org>
2017-06-11 23:35:48 -0400

    adjust interval

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var imgPath = './images/hh.jpg'
 var parser = parse();
 var statements = [];
 var count = 0;
-var interval = 1000*60*1;
+var interval = 1000*60*5;
 var currentWeather = {};
 var sensorNames = {
 	temp: 'temperature',
